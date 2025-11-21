149113f565244257fd24dd1dddc6f5a5194fd4aa
IUB_LOKI <ayoubouharda33@gmail.com>
2025-11-21 09:40:37 +0100

    j'ai un peu organisé mon js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// Une liste des Employee deja predefinie
 let workers = [
     {
-        nom : "Loki",
-        role : "Manager",
-        photo : "/images/alae_img.jpeg",
-        email : "alaeloki18@gmail.com",
-        telephone : "0694733827",
+        nom: "Loki",
+        role: "Manager",
+        photo: "/images/alae_img.jpeg",
+        email: "alaeloki18@gmail.com",
+        telephone: "0694733827",
         zone: null,
         experiences: [
             {
@@ -82,7 +83,7 @@ let workers = [
         ]
     }
 ];
-
+// l'objet des personnes qui ont l'acces a chaque salle
 const autorisations = {
     "Salle de conférence": ["Receptioniste", "agent de securite", "directeur", "Femme de manage", "sucretaire", "Technicien", "responsable RH", "Manager"],
     "Reception": ["Receptioniste"],
@@ -92,41 +93,165 @@ const autorisations = {
     "salle du personnel": ["Receptioniste", "agent de securite", "directeur", "Femme de manage", "sucretaire", "Technicien", "responsable RH", "Manager"]
 };
 // l'appel des fonctions 
-document.addEventListener("DOMContentLoaded", () =>
-{
-    afficherEmployee(workers);
-    affichOptions(workers);
+document.addEventListener("DOMContentLoaded", () => {
+    afficherEmployee();
+    affichOptions();
+    afficherSalle();
 })
 
+// l'affichage des employees 
+function afficherEmployee() {
+    let workList = document.querySelector('.list');
+    workList.innerHTML = '';
+    for (let i = 0; i < workers.length; i++) {
+        let worker = workers[i];
+        if (worker.zone === null) {
+            workList.innerHTML += `
+        <div class="worker_card">
+            <div class="card_header">
+                <img src="${worker.photo}" alt="${worker.nom}" class="user_img">
+            </div>
+            <div class="card_body">
+                <h3>${worker.nom}</h3>
+                <p class="role">${worker.role}</p>
+                <p class="contact">${worker.telephone}</p>
+            </div>
+        </div>
+        `;
+        }
+    }
+}
+
+//
+
+function afficherSalle() {
+    document.querySelectorAll('.occupied-list').forEach(div => div.innerHTML = '');
+
+    workers.forEach((worker, index) => {
+        if (worker.zone !== null) {
+
+            const roomDiv = document.querySelector(`.etageScheme div[data-room="${worker.zone}"]`);
+            if (roomDiv) {
+                const listContainer = roomDiv.querySelector('.occupied-list');
+                listContainer.innerHTML += `
+                    <div class="mini-card">
+                        <img src="${worker.photo}" alt="${worker.nom}">
+                        <h4>${worker.nom}</h4>
+                        
+                        <div class="mini-card-btns">
+                            <button class="btn-info" onclick="voirDescription(${index})">Afficher les infos</button>
+                            <button class="btn-remove" onclick="retirerDeSalle(${index})">Retourner a la liste </button>
+                        </div>
+                    </div>
+                `;
+            }
+        }
+    });
+}
+
+// l'affichage des options 
+function affichOptions() {
+    const allSelects = document.querySelectorAll('.room-select');
+    allSelects.forEach(select => {
+        const roomName = select.getAttribute('data-room');
+        const authorizedWorkers = employeeAutorisee(roomName);
+        select.innerHTML = '<option value="">Selectionner</option>';
+        authorizedWorkers.forEach((worker) => {
+            const workerIndex = workers.indexOf(worker);
+            let option = document.createElement('option');
+            option.value = workerIndex;
+            option.textContent = worker.nom;
+            select.appendChild(option);
+        })
+        select.onchange = function () {
+            const selectedIndex = this.value;
+            if (selectedIndex !== "") {
+                deplacerEmployee(selectedIndex, this.dataset.room);
+            }
+            this.value = "";
+        };
+    })
+}
+
+// fonction dial restrictions
+function employeeAutorisee(roomName) {
+    const authorizedRoles = autorisations[roomName];
+
+    if (!authorizedRoles) {
+        return [];
+    }
+    return workers.filter(worker => {
+        if (worker.zone !== null) {
+            return false;
+        }
+        const role = worker.role;
+        return authorizedRoles.includes(role);
+    });
+}
 
 
+function deplacerEmployee(index, roomName) {
+    if (index === "") return;
+
+    workers[index].zone = roomName;
+
+    afficherEmployee();
+    affichOptions();
+    afficherSalle();
+    alert(workers[index].nom + " est deplacé dans " + roomName);
+}
+
+// retirement men dik zone li fiha 
+function retirerDeSalle(index) {
+    workers[index].zone = null;
+    afficherEmployee();
+    affichOptions();
+    afficherSalle();
+}
+// la fonction qui affiche l modal dial description d'un employee
+function voirDescription(index) {
+    const Emp = workers[index];
+    let message = `
+    Nom: ${Emp.nom}
+    Rôle: ${Emp.role}
+    Email: ${Emp.email}
+    Tel: ${Emp.telephone}
+    Société précedente: ${Emp.experiences[0] ? Emp.experiences[0].societe : 'Aucune'}
+    localisation actuelle: ${Emp.zone}
+    `;
+    let modalDesc = document.getElementById('workerModal');
+    let detailsContainer = document.getElementById('workerDetails');
+    let closeButton = document.getElementsByClassName('close-button-desc')[0];
+    detailsContainer.textContent = message;
+    modalDesc.style.display = "block";
+    closeButton.onclick = function () {
+        modalDesc.style.display = "none";
+    }
+}
+// Modal events
 const modal = document.querySelector('.modal');
 const openBtn = document.getElementById("addEmployee");
 const annulBtn = document.getElementById("closeModal");
 const submitBtn = document.getElementById("submitBtn");
 const closeModal = document.getElementById("closeIcon");
 
-openBtn.addEventListener('click', () =>
-{
+openBtn.addEventListener('click', () => {
     modal.classList.remove("hidden");
 })
-annulBtn.addEventListener('click', () =>
-{
+annulBtn.addEventListener('click', () => {
     modal.classList.add("hidden");
 })
-closeModal.addEventListener('click', () =>
-{
+closeModal.addEventListener('click', () => {
     modal.classList.add("hidden");
 })
 
-// Fonction pour ajouter de nouveaux champs d'expérience
-document.getElementById("experienceBtn").addEventListener("click", function() {
+document.getElementById("experienceBtn").addEventListener("click", function () {
     ajouterChampExperience();
 });
 
 function ajouterChampExperience() {
     const experiencesContainer = document.querySelector('.experiences-container');
-    
+
     const newExperience = document.createElement('div');
     newExperience.className = 'experience-item';
     newExperience.innerHTML = `
@@ -150,11 +275,11 @@ function ajouterChampExperience() {
         </div>
         <button type="button" class="supprimer-experience">Supprimer cette expérience</button>
     `;
-    
+
     experiencesContainer.appendChild(newExperience);
-    
+
     // Ajouter l'événement pour supprimer cette expérience
-    newExperience.querySelector('.supprimer-experience').addEventListener('click', function() {
+    newExperience.querySelector('.supprimer-experience').addEventListener('click', function () {
         experiencesContainer.removeChild(newExperience);
     });
 }
@@ -163,14 +288,14 @@ function ajouterChampExperience() {
 function recupererExperiences() {
     let experienceArray = [];
     let experienceItems = document.querySelectorAll('.experience-item');
-    
-    experienceItems.forEach(function(item) {
+
+    experienceItems.forEach(function (item) {
         let societe = item.querySelector('.exp-societe').value;
         let rolexp = item.querySelector('.exp-role').value;
         let debut = item.querySelector('.exp-du').value;
         let fin = item.querySelector('.exp-a').value;
 
-        if(societe !== '') {
+        if (societe !== '') {
             experienceArray.push({
                 societe: societe,
                 role: rolexp,
@@ -179,16 +304,14 @@ function recupererExperiences() {
             });
         }
     });
-    
+
     return experienceArray;
 }
 
 // Ajouter des employee
 submitBtn.addEventListener('click', ajoutCarte);
-function ajoutCarte()
-{
-    if(workers.length >= 30)
-    {
+function ajoutCarte() {
+    if (workers.length >= 30) {
         alert("vous pouvez pas ajouter encore un employee");
         document.querySelector('.modal').classList.add("hidden");
         return;
@@ -200,162 +323,36 @@ function ajoutCarte()
     let telephone = document.getElementById("telephoneInput").value;
     let experienceArray = recupererExperiences();
 
-if(nom === "" || role === "" || email === "" || telephone ==="")
-{
-    alert("Veuillez saisir les cordonnés s'il vous plait");
-    return;
-}
-let nouveauWorker = {
-    nom: nom,
-    role: role,
-    photo: photo !== "" ? photo: "https://www.w3schools.com/howto/img_avatar.png",
-    email: email,
-    telephone : telephone,
-    zone: null,
-    experiences: experienceArray
-}
-workers.push(nouveauWorker);
-afficherEmployee();
-let allInputs = document.querySelectorAll('.modal input');
-
-allInputs.forEach(input => {
-    input.value = "";
-});
-const experiencesContainer = document.querySelector('.experiences-container');
-const firstExperience = experiencesContainer.querySelector('.experience-item');
-experiencesContainer.innerHTML = '';
-experiencesContainer.appendChild(firstExperience);
-
-// Réinitialiser les inputs du premier champ d'expérience
-firstExperience.querySelector('.exp-societe').value = '';
-firstExperience.querySelector('.exp-role').value = '';
-firstExperience.querySelector('.exp-du').value = '';
-firstExperience.querySelector('.exp-a').value = '';
-document.querySelector('.modal').classList.add("hidden");
-}
-
-
-
-// Afficher les employee dans la liste d'attente
-function afficherEmployee(){
-    let workList = document.querySelector('.list');
-    workList.innerHTML = '';
-    for(let i =0; i <  workers.length ; i++)
-    {
-        let worker = workers[i];
-        if(worker.zone ===null){
-        workList.innerHTML += `
-        <div class="worker_card">
-            <div class="card_header">
-                <img src="${worker.photo}" alt="${worker.nom}" class="user_img">
-            </div>
-            <div class="card_body">
-                <h3>${worker.nom}</h3>
-                <p class="role">${worker.role}</p>
-                <p class="contact">${worker.telephone}</p>
-            </div>
-        </div>
-        `;
+    if (nom === "" || role === "" || email === "" || telephone === "") {
+        alert("Veuillez saisir les cordonnés s'il vous plait");
+        return;
     }
-}
-}
-
-function supprEmployee()
-{
-//khassni mazal nerje3 liha nekhdem 3la suppresion
-
-}
-function affichOptions()
-{
-    const allSelects = document.querySelectorAll('.room-select');
-    allSelects.forEach(select => 
-    {
-        const roomName = select.getAttribute('data-room');
-        const authorizedWorkers = employeeAutorisee(roomName); 
-        select.innerHTML = '<option value="">Selectionner</option>';
-        authorizedWorkers.forEach((worker) => 
-        {
-            const workerIndex = workers.indexOf(worker);
-            let option = document.createElement('option');
-            option.value = workerIndex; 
-            option.textContent = worker.nom;
-            select.appendChild(option);
-        })
-        select.onchange = function() {
-            const selectedIndex = this.value;
-            if(selectedIndex !== "") {
-               deplacerEmployee(selectedIndex, this.dataset.room);
-           }
-           this.value = "";
-        };
-    })
-}
-
-
-function deplacerEmployee(index,roomName)
-{
-    if(index === "") return;
-
-    workers[index].zone = roomName;
-
+    let nouveauWorker = {
+        nom: nom,
+        role: role,
+        photo: photo !== "" ? photo : "https://www.w3schools.com/howto/img_avatar.png",
+        email: email,
+        telephone: telephone,
+        zone: null,
+        experiences: experienceArray
+    }
+    workers.push(nouveauWorker);
     afficherEmployee();
-    affichOptions();
-    afficherSalle();
-    alert(workers[index].nom + " est deplacé dans " + roomName);
-}
+    let allInputs = document.querySelectorAll('.modal input');
 
-function afficherSalle() {
-    document.querySelectorAll('.occupied-list').forEach(div => div.innerHTML = '');
-
-    workers.forEach((worker, index) => {
-        if (worker.zone !== null) {
-            
-            const roomDiv = document.querySelector(`.etageScheme div[data-room="${worker.zone}"]`);
-            if (roomDiv) {
-                const listContainer = roomDiv.querySelector('.occupied-list');
-                listContainer.innerHTML += `
-                    <div class="mini-card">
-                        <img src="${worker.photo}" alt="${worker.nom}">
-                        <h4>${worker.nom}</h4>
-                        
-                        <div class="mini-card-btns">
-                            <button class="btn-info" onclick="voirDescription(${index})">Afficher les infos</button>
-                            <button class="btn-remove" onclick="retirerDeSalle(${index})">Retourner a la liste </button>
-                        </div>
-                    </div>
-                `;
-            }
-        }
+    allInputs.forEach(input => {
+        input.value = "";
     });
-}
-// retirement men dik zone li fiha 
-function retirerDeSalle(index) {
-    workers[index].zone = null;
-    afficherEmployee();
-    affichOptions();
-    afficherSalle();
-}
-// la fonction qui affiche l modal dial description d'un employee
-function voirDescription(index)
-{
-    const Emp = workers[index];
-    let message = `
-    Nom: ${Emp.nom}
-    Rôle: ${Emp.role}
-    Email: ${Emp.email}
-    Tel: ${Emp.telephone}
-    Société précedente: ${Emp.experiences[0] ? Emp.experiences[0].societe : 'Aucune'}
-    localisation actuelle: ${Emp.zone}
-    `;
-    let modalDesc = document.getElementById('workerModal');
-    let detailsContainer = document.getElementById('workerDetails');
-    let closeButton = document.getElementsByClassName('close-button-desc')[0];
-    detailsContainer.textContent = message;
-    modalDesc.style.display = "block";
-    closeButton.onclick = function() 
-    {
-        modalDesc.style.display = "none";
-    }
+    const experiencesContainer = document.querySelector('.experiences-container');
+    const firstExperience = experiencesContainer.querySelector('.experience-item');
+    experiencesContainer.innerHTML = '';
+    experiencesContainer.appendChild(firstExperience);
+
+    firstExperience.querySelector('.exp-societe').value = '';
+    firstExperience.querySelector('.exp-role').value = '';
+    firstExperience.querySelector('.exp-du').value = '';
+    firstExperience.querySelector('.exp-a').value = '';
+    document.querySelector('.modal').classList.add("hidden");
 }
 
 // Validation Regex
@@ -393,28 +390,13 @@ emailInput.addEventListener("input", () => {
 });
 
 // Validation Téléphone
-telephoneInput.addEventListener("input", () => {
-    if (!phoneRegex.test(telephoneInput.value)) {
-        telephoneError.textContent = "Numéro invalide (Format marocain requis).";
-        telephoneInput.style.borderColor = "red";
-    } else {
-        telephoneError.textContent = "";
-        telephoneInput.style.borderColor = "green";
-    }
-});
+// telephoneInput.addEventListener("input", () => {
+//     if (!phoneRegex.test(telephoneInput.value)) {
+//         telephoneError.textContent = "Numéro invalide (Format marocain requis).";
+//         telephoneInput.style.borderColor = "red";
+//     } else {
+//         telephoneError.textContent = "";
+//         telephoneInput.style.borderColor = "green";
+//     }
+// });
 
-// fonction dial restrictions
-function employeeAutorisee(roomName) {
-    const authorizedRoles = autorisations[roomName]; 
-
-    if (!authorizedRoles) {
-        return [];
-    }
-    return workers.filter(worker => {
-        if (worker.zone !== null) {
-            return false;
-        }
-        const role = worker.role;
-        return authorizedRoles.includes(role);
-    });
-}
